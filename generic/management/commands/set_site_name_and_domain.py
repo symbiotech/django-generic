7f5d62036bf860c7fb88e5285555e87cf08dbf1f
Simon Meers <simon@simonmeers.com>
2012-05-23 12:09:37 +1000

    More ssnad tweaks

--- generic/management/commands/set_site_name_and_domain.py
+++ generic/management/commands/set_site_name_and_domain.py
@@ -6,11 +6,11 @@ class Command(BaseCommand):
     help = 'Set Site name and domain from settings.SITE_NAME/DOMAIN'
     def handle(self, *args, **options):
         if Site.objects.count() > 1:
-            raise CommandError("Error: Must have only one Site.\n")
+            raise CommandError("Must have only one Site.\n")
         Site.objects.get_or_create(
             defaults={
                 'domain':settings.SITE_DOMAIN,
                 'name':settings.SITE_NAME})
         self.stdout.write(
-            u'Site details set:\n   {0.name}s / {0.domain}s\n'.format(
+            u'Site details set:\n   {0.name} / {0.domain}\n'.format(
                 Site.objects.all()[0]))
