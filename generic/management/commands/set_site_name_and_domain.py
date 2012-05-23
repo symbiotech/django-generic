9815ea4e37ec105001f4c5ccfce77ede1aad9edc
Simon Meers <simon@simonmeers.com>
2012-05-23 11:57:29 +1000

    set_site_name_and_domain tweaks

--- generic/management/commands/set_site_name_and_domain.py
+++ generic/management/commands/set_site_name_and_domain.py
@@ -5,10 +5,12 @@ from django.core.management.base import BaseCommand, CommandError
 class Command(BaseCommand):
     help = 'Set Site name and domain from settings.SITE_NAME/DOMAIN'
     def handle(self, *args, **options):
-        if Site.objects.count() != 1:
+        if Site.objects.count() > 1:
             raise CommandError("Error: Must have only one Site.\n")
-        Site.objects.update(
-            domain=settings.SITE_DOMAIN, name=settings.SITE_NAME)
+        Site.objects.get_or_create(
+            defaults={
+                'domain':settings.SITE_DOMAIN,
+                'name':settings.SITE_NAME})
         self.stdout.write(
-            u'Site details set:\n   %(name)s / %(domain)s\n' % (
-                Site.objects.all()[0].__dict__))
+            u'Site details set:\n   {0.name}s / {0.domain}s\n'.format(
+                Site.objects.all()[0]))
