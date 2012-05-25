e3033a9d73400c8faadbe7ff4460675685fc68ec
Simon Meers <simon@simonmeers.com>
2012-05-25 11:32:27 +1000

    SITE_NAME_TRANSLATED

--- generic/context_processors.py
+++ generic/context_processors.py
@@ -1,4 +1,7 @@
 from django.conf import settings
+from django.utils.translation import ugettext_lazy as _
 
 def generic(request=None):
-    return settings.TEMPLATE_CONSTANTS
+    return dict(
+        settings.TEMPLATE_CONSTANTS,
+        SITE_NAME_TRANSLATED=_(settings.SITE_NAME))
