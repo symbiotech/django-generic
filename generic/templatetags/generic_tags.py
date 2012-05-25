eb79a1629d35f65ed4e789b8f6e631cfcf3be85c
Simon Meers <simon@simonmeers.com>
2012-05-25 11:37:40 +1000

    linkify filter

--- generic/templatetags/generic_tags.py
+++ generic/templatetags/generic_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.utils.safestring import mark_safe
 register = template.Library()
 
 @register.inclusion_tag('_field.html')
@@ -8,3 +9,7 @@ def field(field, *args, **kwargs):
         'show_label': kwargs.get('show_label', True),
         'label_override': kwargs.get('label_override', None),
         }
+
+@register.filter
+def linkify(obj):
+    return mark_safe('<a href="%s">%s</a>' % (obj.get_absolute_url(), obj))
