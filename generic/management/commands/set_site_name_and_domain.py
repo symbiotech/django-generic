0ec94d9208f643560fa888239e1fad7d8ba7da4e
Simon Meers <simon@simonmeers.com>
2012-06-03 00:48:54 +1000

    Correct flawed get_or_create thinking

--- generic/management/commands/set_site_name_and_domain.py
+++ generic/management/commands/set_site_name_and_domain.py
@@ -5,12 +5,18 @@ from django.core.management.base import BaseCommand, CommandError
 class Command(BaseCommand):
     help = 'Set Site name and domain from settings.SITE_NAME/DOMAIN'
     def handle(self, *args, **options):
-        if Site.objects.count() > 1:
+        try:
+            site = Site.objects.get()
+        except Site.DoesNotExist:
+            site = Site.objects.create(
+                domain=settings.SITE_DOMAIN,
+                name=settings.SITE_NAME)
+        except Site.MultipleObjectsReturned:
             raise CommandError("Must have only one Site.\n")
-        Site.objects.get_or_create(
-            defaults={
-                'domain':settings.SITE_DOMAIN,
-                'name':settings.SITE_NAME})
+        else:
+            site.domain = settings.SITE_DOMAIN
+            site.name = settings.SITE_NAME
+            site.save()
+
         self.stdout.write(
-            u'Site details set:\n   {0.name} / {0.domain}\n'.format(
-                Site.objects.all()[0]))
+            u'Site details set:\n   {0.name} / {0.domain}\n'.format(site))
