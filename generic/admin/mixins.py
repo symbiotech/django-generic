eb0d51961d190bbb5a98d3cde864251011b0396f
Simon Meers <simon@simonmeers.com>
2013-03-19 14:14:38 +1100

    CookedIdWidgets, get 'em while they're hot.

--- generic/admin/mixins.py
+++ generic/admin/mixins.py
@@ -1,8 +1,100 @@
 from django import http
+from django.conf import settings
 from django.contrib import admin
-from django.conf.urls.defaults import patterns, url
+from django.conf.urls import patterns, url
+from django.core.exceptions import ImproperlyConfigured
 from django.shortcuts import get_object_or_404, redirect
+
+try:
+    import json
+except ImportError:
+    from django.utils import simplejson as json
+
 from .. import models
+from .widgets import ForeignKeyCookedIdWidget, ManyToManyCookedIdWidget
+
+class CookedIdAdmin(admin.ModelAdmin):
+    """
+    Support for CookedIdWidgets (vs. RawIdWidgets) in admin.
+
+    See:
+    - `generic.admin.widgets.ManyToManyCookedIdWidget`, and
+    - `generic.admin.widgets.ForeignKeyCookedIdWidget`
+
+    Simply list fields in self.cooked_id_widgets instead of self.raw_id_widgets
+
+    Override self.cook() to customise cooked object representations.
+    """
+    cooked_id_fields = ()
+
+    def cook(self, obj, request, field_name):
+        """
+        Override this to customise the "cooked" representation of objects
+        """
+        return unicode(obj)
+
+    def cook_ids(self, request, field_name, raw_ids):
+        # TODO: extend to support non-integer/non-`id` PKs
+        if not field_name in self.cooked_id_fields:
+            raise http.Http404
+        try:
+            ids = map(int, raw_ids.split(','))
+        except ValueError:
+            if raw_ids == '':
+                ids = []
+            else:
+                raise http.Http404
+        target_model_admin = self.admin_site._registry.get(
+            self.model._meta.get_field(field_name).rel.to)
+        response_data = {}
+        if target_model_admin:
+            for obj in target_model_admin.queryset(request).filter(id__in=ids):
+                response_data[obj.pk] = self.cook(
+                    obj, request=request, field_name=field_name)
+        else:
+            pass # graceful-ish.
+        return http.HttpResponse(
+            json.dumps(response_data), mimetype='application/json')
+
+    def get_urls(self):
+        return patterns(
+            '',
+            url(r'^cook-ids/(?P<field_name>\w+)/(?P<raw_ids>.*)/$',
+                self.admin_site.admin_view(self.cook_ids))
+        ) + super(CookedIdAdmin, self).get_urls()
+
+    def assert_cooked_target_admin(self, db_field):
+        if db_field.rel.to in self.admin_site._registry:
+            return True
+        else:
+            if settings.DEBUG:
+                raise ImproperlyConfigured(
+                    "%s.cooked_id_fields contains '%r', but %r "
+                    "is not registed in the same admin site." % (
+                        self.__class__.__name__,
+                        db_field.name,
+                        db_field.rel.to,
+                    )
+                )
+            else:
+                pass # fail silently
+
+    def formfield_for_manytomany(self, db_field, request=None, **kwargs):
+        if db_field.name in self.cooked_id_fields:
+            if self.assert_cooked_target_admin(db_field):
+                kwargs['widget'] = ManyToManyCookedIdWidget(
+                    db_field.rel, self.admin_site)
+        return super(CookedIdAdmin, self).formfield_for_manytomany(
+            db_field, request=request, **kwargs)
+
+    def formfield_for_foreignkey(self, db_field, request=None, **kwargs):
+        if db_field.name in self.cooked_id_fields:
+            if self.assert_cooked_target_admin(db_field):
+                kwargs['widget'] = ForeignKeyCookedIdWidget(
+                    db_field.rel, self.admin_site)
+        return super(CookedIdAdmin, self).formfield_for_foreignkey(
+            db_field, request=request, **kwargs)
+
 
 class DelibleAdmin(admin.ModelAdmin):
     """ Admin with "undelete" functionality for Delible objects """
---
+++ generic/static/generic/js/cooked_id_widgets.js
@@ -0,0 +1,78 @@
+(
+    function($){
+        $(document).ready(function(){
+            window.update_cooked_field = function(field){
+                $(field).hide();
+                var container = $(field).closest('div');
+                $('.help', container).html(
+                    'Click cross icons to remove existing items, ' +
+                    'or magnifying glass icon to add more.'
+                );
+                var field_name = $(field).attr('name');
+                var ids = escape($(field).val());
+                var cook_url = '../cook-ids/' + field_name + '/' + ids + '/';
+                $.get(cook_url, function(data){
+                    var cooked = $('.cooked-data', container);
+                    cooked.html('');
+                    $.each(data, function(key, name){
+                        $('<li data-id="'+key+'"></li>').text(name).append(
+                            ' <a onclick="remove_cooked_item();"' +
+                            ' title="remove">&nbsp;</a>'
+                        ).appendTo(cooked);
+                    });
+                });
+            }
+
+            window.remove_cooked_item = function(){
+                var li = $(event.target).parent();
+                var id_to_remove = $(li).attr('data-id'); // jQuery only 1.4.2
+                var container = $(li).closest('div');
+                var field = $('.CookedIdField', container);
+                var values = $(field).val().split(',');
+                $(field).val(
+                    $.grep(
+                        values, function(id){ return id != id_to_remove }
+                    ).join(',')
+                );
+                update_cooked_field(field);
+            }
+
+            var originalDismissRelated = window.dismissRelatedLookupPopup;
+            window.dismissRelatedLookupPopup = function(win, chosenId){
+                originalDismissRelated(win, chosenId);
+                $(window).trigger('dismissRelatedLookupPopup');
+            }
+
+            var originalDismissAddAnother = window.dismissAddAnotherPopup;
+            window.dismissAddAnotherPopup = function(win, newId, newRepr){
+                originalDismissAddAnother(win, newId, newRepr);
+                $(window).trigger('dismissAddAnotherPopup');
+            }
+
+            $('.CookedIdField').each(
+                function(index, element){
+                    update_cooked_field(element);
+                    $(element).bind(
+                        'change', function(event){
+                            update_cooked_field(event.target);
+                        }
+                    );
+                }
+            );
+            $(window).bind('dismissRelatedLookupPopup', function(event){
+                $('.CookedIdField').each(
+                    function(index, element){
+                        $(element).triggerHandler('change');
+                    }
+                );
+            });
+            $(window).bind('dismissAddAnotherPopup', function(event){
+                $('.CookedIdField').each(
+                    function(index, element){
+                        $(element).triggerHandler('change');
+                    }
+                );
+            });
+        });
+    }
+)(django.jQuery);
