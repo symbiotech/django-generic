a3023804bfe191662fce951441303f720df243f2
Simon Meers <simon@simonmeers.com>
2013-04-19 13:46:25 +1000

    extra_fields for create_(super)user

--- generic/user/email_based/models.py
+++ generic/user/email_based/models.py
@@ -5,17 +5,18 @@ from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
 
 class EmailBasedUserManager(BaseUserManager):
-    def create_user(self, email, password=None):
+    def create_user(self, email, password=None, **extra_fields):
         if not email:
             raise ValueError('Users must have an email address')
-        user = self.model(email=self.normalize_email(email))
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         # TODO: handle email address clashes
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password):
-        user = self.create_user(email, password=password)
+    def create_superuser(self, email, password, **extra_fields):
+        user = self.create_user(email, password=password, **extra_fields)
+        user.is_active = True
         user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
