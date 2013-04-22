5e1c0e476c9b056c994be9be5af4f28d5fc40436
Simon Meers <simon@simonmeers.com>
2013-04-22 16:25:05 +1000

    correct string formats in get_token

--- generic/utils/tokens.py
+++ generic/utils/tokens.py
@@ -4,5 +4,5 @@ from django.conf import settings
 def get_token(shortness=5, **params):
     content = settings.SECRET_KEY
     for key in sorted(params.keys()):
-        content += '{0:s}={1:s}'.format(key, params[key])
+        content += '{0!s}={1!s}'.format(key, params[key])
     return hashlib.sha1(content).hexdigest()[::shortness]
