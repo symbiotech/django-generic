fe67a08793fdf7d58d0dbba5092159af8a674ce0
Simon Meers <simon@simonmeers.com>
2013-06-01 21:50:37 +1000

    @cache_method

--- generic/decorators.py
+++ generic/decorators.py
@@ -1,7 +1,62 @@
 from functools import wraps
 from django.conf import settings
+from django.core.cache import cache, get_cache
 from django.http import HttpResponse
 from django.utils import simplejson
+
+import logging
+logger = logging.getLogger(__name__)
+
+def cache_method(cache_name=None):
+    """
+    Caches the result of a method for its object instance using the passed
+    arguments to generate a cache key
+    """
+    def inner(method):
+        @wraps(method)
+        def wrapped_method(self, *args, **kwargs):
+            force_reload = kwargs.pop('force_reload', False)
+            cache_obj = get_cache(cache_name) if cache_name else cache
+            cache_key = 'generic-%s-%s-%s' % (
+                self.__class__.__name__,
+                self.pk,
+                method.__name__,
+            )
+            for value in args:
+                if isinstance(value, (list, tuple)):
+                    value = ','.join(map(unicode, value))
+                cache_key += '-%s' % value
+            for key, value in kwargs.iteritems():
+                if isinstance(value, (list, tuple)):
+                    value = ','.join(map(unicode, value))
+                cache_key += '-%s=%s' % (key, value)
+            debug_info = [cache_key]
+
+            if force_reload:
+                result = method(self, *args, **kwargs)
+                cache_obj.set(cache_key, result)
+                debug_info.append('forced reload')
+            else:
+                try:
+                    result = cache_obj.get(cache_key)
+                except Exception as e:
+                    logger.warning('Cache error: {0}'.format(e))
+                    result = None
+                if result is None:
+                    if not cache_obj.has_key(cache_key):
+                        debug_info.append('miss')
+                        result = method(self, *args, **kwargs)
+                        cache_obj.set(cache_key, result)
+                    else:
+                        pass # in the cache, but result is None
+                else:
+                    debug_info.append('hit')
+            debug_info.append(result)
+            logger.debug(u' -- '.join(debug_info))
+            return result
+        return wrapped_method
+    return inner
+
 
 def cache_result_in_instance(method):
     """
