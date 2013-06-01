b32c4c7f01aac7bae6934715bcdb70c258b7990e
Simon Meers <simon@simonmeers.com>
2013-06-01 22:00:06 +1000

    don't assume everything is a string

--- generic/decorators.py
+++ generic/decorators.py
@@ -52,7 +52,7 @@ def cache_method(cache_name=None):
                 else:
                     debug_info.append('hit')
             debug_info.append(result)
-            logger.debug(u' -- '.join(debug_info))
+            logger.debug(u' -- '.join(map(unicode, debug_info)))
             return result
         return wrapped_method
     return inner
