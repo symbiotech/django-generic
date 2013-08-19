b64324b154daa1c6217dd7b63d6908d753985d6b
Aram Dulyan <aram00@gmail.com>
2013-08-19 17:16:28 +1000

    Restored Relatable code after pull request

---
+++ generic/mixins.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+class Inheritable(object):
+    def get_leaf_object(self):
+        """Returns the model instance object as instance of the outermost leaf class,
+        by searching through its related descriptors to find the right model."""
+        for obj in self._meta.get_all_related_objects():
+            if obj.model != self.__class__ and obj.field.rel.parent_link:
+                try:
+                    return getattr(self, obj.get_accessor_name()).get_leaf_object()
+                except obj.model.DoesNotExist:
+                    pass
+        return self
+
+    def __unicode__(self):
+        # Display the unicode representation of the leaf model instance
+        leaf = self.get_leaf_object()
+        if leaf.__class__ != self.__class__:
+            return "%s: %s" % (leaf._meta.verbose_name, unicode(leaf))
+        # Simulate what Django does in Model.__str__, so that when no
+        # __unicode__ method is defined on the model, this mixin doesn't
+        # affect the unicode representation
+        if hasattr(super(Inheritable, self), '__unicode__'):
+            return super(Inheritable, self).__unicode__()
+        return '%s object' % self.__class__.__name__
+
+
+class HousekeepingMixin(models.Model):
+    """Abstract mixin class to collect creation and update timestamps."""
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    deleted = models.BooleanField(default=False)
+
+    class Meta:
+        abstract = True
