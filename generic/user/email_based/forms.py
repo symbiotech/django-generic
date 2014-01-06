4aa8a24dfb3b2bd613538197f17fd8eadef0b180
Aram Dulyan <aram00@gmail.com>
2014-01-06 14:47:54 +1100

    Marked user registration form labels for translation.

--- generic/user/email_based/forms.py
+++ generic/user/email_based/forms.py
@@ -1,11 +1,12 @@
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.utils.translation import ugettext_lazy as _
 
 class UserCreationForm(forms.ModelForm):
-    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password1 = forms.CharField(label=_('Password'), widget=forms.PasswordInput)
     password2 = forms.CharField(
-        label='Password confirmation', widget=forms.PasswordInput)
+        label=_('Password confirmation'), widget=forms.PasswordInput)
 
     class Meta:
         model = get_user_model()
@@ -15,7 +16,7 @@ class UserCreationForm(forms.ModelForm):
         password1 = self.cleaned_data.get('password1')
         password2 = self.cleaned_data.get('password2')
         if password1 and password2 and password1 != password2:
-            raise forms.ValidationError("Passwords don't match")
+            raise forms.ValidationError(_('Passwords don\'t match'))
         return password2
 
     def save(self, commit=True):
