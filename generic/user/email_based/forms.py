5fd3c14cf7af6a62418ce14920a82f78718de0ab
Simon Meers <simon@simonmeers.com>
2014-05-15 14:03:17 +1000

    Make email_based.UserChangeForm compatible with Django 1.5 again

--- generic/user/email_based/forms.py
+++ generic/user/email_based/forms.py
@@ -1,3 +1,4 @@
+import django
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
@@ -37,7 +38,7 @@ class UserChangeForm(forms.ModelForm):
 
     class Meta:
         model = get_user_model()
-        fields = '__all__'
+        fields = '__all__' if django.VERSION >= (1,6) else None
 
     def clean_password(self):
         return self.initial['password']
